9b07139319e7a758d61f10c132f4f4d53140d93a
mrfrac <fracturer@gmail.com>
2022-04-18 10:41:15 +0500

    Доработки по главной странице

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,10 +1,22 @@
 (function () {
   const button = document.querySelector(".main-nav__toggle");
+  const mainNav = document.querySelector(".main-nav");
 
-  if (button) {
-    const navigationElement = document.querySelector(".main-nav__list");
+  if (mainNav) {
+    mainNav.classList.remove("main-nav--nojs");
+  }
+
+  if (button && mainNav) {
     button.addEventListener("click", () => {
-      navigationElement.classList.toggle("main-nav__list--open");
+      if (mainNav.classList.contains("main-nav--opened")) {
+        console.log("close")
+        mainNav.classList.remove("main-nav--opened")
+        mainNav.classList.add("main-nav--closed");
+      } else {
+        console.log("open")
+        mainNav.classList.remove("main-nav--closed")
+        mainNav.classList.add("main-nav--opened");
+      }
     });
   }
 })();
